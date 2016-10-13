c66469efaea71afc5483764162c234ca49c2857c
Henry Doan <henrydoan96@gmail.com>
2016-10-12 20:24:14 -0600

    DRY code and tweaked results page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,3 +1,4 @@
+
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
@@ -5,11 +6,12 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
-	return props.isLoading === true
-		? <p>LOADING</p>
-		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+  return props.isLoading === true
+    ? <p>LOADING</p>
+    : <MainContainer>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
           <UserDetailsWrapper header='Player 1'>
@@ -21,25 +23,21 @@ function ConfirmBattle (props) {
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-            	Initiate Battle!
-            </button>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
           </div>
           <div className='col-sm-12' style={styles.space}>
             <Link to='/playerOne'>
-              <button type='button' className='btn btn-lg btn-danger'>
-              	Reselect Players
-              </button>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
             </Link>
           </div>
         </div>
-      </div>
+      </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
-	isLoading: PropTypes.bool.isRequired,
-	onInitiateBattle: PropTypes.func.isRequired,
-	playersInfo: PropTypes.array.isRequired
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isRequired,
 }
 
 module.exports = ConfirmBattle;
